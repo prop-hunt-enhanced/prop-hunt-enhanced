7132c98e2eafe3e8bf68e357d3abe86d2a537117
Phyremaster <phyremaster@hotmail.com>
2019-10-16 04:05:33 -0400

    Prevent hunter explosive self damage

--- gamemodes/prop_hunt/gamemode/init.lua
+++ gamemodes/prop_hunt/gamemode/init.lua
@@ -224,6 +224,10 @@ function EntityTakeDamage(ent, dmginfo)
 			hook.Call("PH_HunterDeathPenalty", nil, att)
 		end
 	end
+
+    if GAMEMODE:InRound() && ent && ent:IsPlayer() && ent:Alive() && ent:Team() == TEAM_HUNTERS && att && att:IsPlayer() && att:Team() == TEAM_HUNTERS && dmginfo:IsExplosionDamage() then
+        return true
+    end
 end
 hook.Add("EntityTakeDamage", "PH_EntityTakeDamage", EntityTakeDamage)
 
